3eb008f257f4c0f67804eb2e0195d1357897ffab
rahman <abduronikrahman@gmail.com>
2023-02-13 15:58:21 -0600

    Send email and email confirmation during registration.

---
+++ verify.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'database/db_connect.php';
+ $verification_code = $_GET['code'];
+
+
+// Update the database column
+$sql = "UPDATE users SET email_verification	 = 1 WHERE trim(email) = trim('$verification_code')";
+
+if (mysqli_query($conn, $sql)) {
+    echo 'Your email address has been verified. Thank you!';
+} else {
+    echo 'An error occurred. Please try again later.';
+}
+
+mysqli_close($conn);
+?>
