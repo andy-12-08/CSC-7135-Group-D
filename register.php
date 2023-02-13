32498b0fe1d9d6801aef16b855dbd97a3d645da5
Andrew Okafor <104956398+andy-12-08@users.noreply.github.com>
2023-02-13 16:30:34 -0600

    Update register.php

--- register.php
+++ register.php
@@ -30,6 +30,7 @@
             if (mysqli_query($conn, $sql)) {
             echo "New record created successfully";
             send_email($username, $username);
+             header("Location: index.php");
             } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
