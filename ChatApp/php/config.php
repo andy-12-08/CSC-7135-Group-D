6e3e4a39a2058d36f2cbf89c82f6944adaaee1b3
rahman <abduronikrahman@gmail.com>
2023-03-29 04:00:44 -0500

    online-merge-databse

--- ChatApp/php/config.php
+++ ChatApp/php/config.php
@@ -1,8 +1,8 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "doctor_appointment";
+  $hostname = "mysqldatabase.cncyjoyafhkj.us-east-1.rds.amazonaws.com";
+  $username = "admin";
+  $password = "SEgroupd";
+  $dbname = "tutor_online";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
