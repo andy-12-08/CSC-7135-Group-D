748a3b2ecf4f004a161679a18402a2733613bd76
Andrew Okafor <aokafo3@lsu.edu>
2023-09-20 15:13:00 -0500

    changes

--- register.php
+++ register.php
@@ -1,3 +1,24 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="csslogin/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="csslogin/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="csslogin/style.css">
+
+    <title>TutorOnline</title>
+  </head>
+
+
+
 <?php
 include('header.php');
 include('class/Appointment.php');
